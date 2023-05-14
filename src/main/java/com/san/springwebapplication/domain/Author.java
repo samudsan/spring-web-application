33d81f225e4e85ace5829aa8d66c621c1260a7c7
Sandeep <samudsan2020@gmail.com>
2023-05-14 18:28:36 +0530

    developed jpa entities books and authors

---
+++ src/main/java/com/san/springwebapplication/domain/Author.java
@@ -0,0 +1,62 @@
+package com.san.springwebapplication.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToManyfh
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+
+
+
+}
