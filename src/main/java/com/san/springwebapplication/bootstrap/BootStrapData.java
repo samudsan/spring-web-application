17fe07922d64b38d223bb8feb64d577280090fcc
Sandeep <samudsan2020@gmail.com>
2023-05-15 00:21:02 +0530

    publish code added

--- src/main/java/com/san/springwebapplication/bootstrap/BootStrapData.java
+++ src/main/java/com/san/springwebapplication/bootstrap/BootStrapData.java
@@ -23,34 +23,37 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Author author  = new Author("Goggins","john");
-        Book book =  new Book("Never Finished","123");
+        // Create first author, book and publisher
+        Author author1 = new Author("Goggins", "John");
+        Book book1 = new Book("Never Finished", "123");
         Publisher publisher1 = new Publisher("Goa", 231564L);
-        book.setPublisher(publisher1);
-        author.getBooks().add(book);
-        book.getAuthors().add(author);
-        publisher1.getBooks().add(book);
-
-
-        authorRepository.save(author);
-        bookRepository.save(book);
         publisherRepository.save(publisher1);
+        book1.setPublisher(publisher1);
+        author1.getBooks().add(book1);
+        book1.getAuthors().add(author1);
+        publisher1.getBooks().add(book1);
 
-        Author author2  = new Author("Goggins","john");
-        Book book2 =  new Book("you can't hurt me","123");
-        Publisher publisher2 = new Publisher("Goa", 231556);
+        authorRepository.save(author1);
+        bookRepository.save(book1);
+
+
+        // Create second author, book and publisher
+        Author author2 = new Author("Smith", "Sarah");
+        Book book2 = new Book("You Can't Hurt Me", "456");
+        Publisher publisher2 = new Publisher("London", 231556L);
+        publisherRepository.save(publisher2);
         book2.setPublisher(publisher2);
         author2.getBooks().add(book2);
         book2.getAuthors().add(author2);
         publisher2.getBooks().add(book2);
+
         authorRepository.save(author2);
+
         bookRepository.save(book2);
-        publisherRepository.save(publisher2);
 
 
         System.out.println("Started in BootStrap Class");
-        System.out.println("Number of books : "+bookRepository.count());
-        System.out.println("Number of  books assigned to publisher: "+publisherRepository.count());
-
+        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of books assigned to a publisher: " + publisherRepository.count());
     }
 }
